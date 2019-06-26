5badc3eeafb33157802acd258c6efa5b0b318176
Giacomo Sorbi <giacomo.sorbi@bjss.com>
2019-06-26 17:56:42 +0100

    Updates

---
+++ precache-manifest.2fe62e6ec3c1fe58892cdd71d95569ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89e152725036e845db61f7164b1e7fb3",
+    "url": "/pomodoro-timer/index.html"
+  },
+  {
+    "revision": "3aca6f4f078f3156b8a0",
+    "url": "/pomodoro-timer/static/css/main.69b5e9c8.chunk.css"
+  },
+  {
+    "revision": "b519ccf92e06c6ec7369",
+    "url": "/pomodoro-timer/static/js/2.e5f60b21.chunk.js"
+  },
+  {
+    "revision": "3aca6f4f078f3156b8a0",
+    "url": "/pomodoro-timer/static/js/main.e1d18104.chunk.js"
+  },
+  {
+    "revision": "8d84c672a99e0f0ece83",
+    "url": "/pomodoro-timer/static/js/runtime~main.2a0df6ba.js"
+  },
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/pomodoro-timer/static/media/calculator.1e670d88.ttf"
+  }
+]);
